655b3d9856008c46d03374027dd9c6a61a14fb6b
Aditya Mukherjee <64586387+Adi2106@users.noreply.github.com>
2021-06-11 23:11:20 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  var typed = new Typed(".typing", {
+    strings: ["A Full Stack Web Developer", "A Freelancer", "An Ethical Hacker", "A Content Writer "],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+  });
+});
