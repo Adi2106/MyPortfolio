b92b8f20789088f57d02c8b9eba6ac4cbaba8072
Aditya Mukherjee <64586387+Adi2106@users.noreply.github.com>
2021-09-27 14:46:32 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   var typed = new Typed(".typing", {
-    strings: ["A Web Developer", "A Freelancer", "A Software Developer ", "An Ethical Hacker" ],
+    strings: ["A Web Developer", "A Freelancer", "A Software Developer(JAVA)" ],
     typeSpeed: 100,
     backSpeed: 60,
     loop: true
